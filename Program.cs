bfbaea6b8b03bf6f8df9a190f2c92b0e7419248b
DESKTOP-PI164O8\Admin <pearmonday>
2021-04-24 15:43:17 +0700

    ข้อ2.1 ตอบ ERROR ข้อ2.2 ตอบ ผลลัพธ์เหมือนเดิม เพราะเงื่อนไขไม่ต่างกัน

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace midterm_sat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ข้อ2.1/2.2
+            Console.Write("input a number =");
+            int year =int.Parse(Console.ReadLine());
+            
+            if ((year % 400) == 0)
+            {
+                Console.Write("Leap year!");
+            }
+            else if ((year % 100) == 0)
+            {
+                Console.Write("Not leap year!");
+            }
+            else if ((year % 4) == 0)
+            {
+                Console.Write("Leap year!");
+            }
+            else
+                Console.Write("Error!!");
+            
+        }
+    }
+}

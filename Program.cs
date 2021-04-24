743dde6193ecc02c17aa70645b5ec26ade7e9fa5
DESKTOP-PI164O8\Admin <pearmonday>
2021-04-24 16:07:03 +0700

    ข้อ3

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace midterm_sat
         static void Main(string[] args)
         {
             //ข้อ2.1/2.2
+            /*
             Console.Write("input a number =");
             int year =int.Parse(Console.ReadLine());
             
@@ -24,7 +25,49 @@ namespace midterm_sat
             }
             else
                 Console.Write("Error!!");
-            
+            */
+            //ข้อ3
+            float x,y,dx,dy,step;
+            Console.Write("Input x1");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.Write("Input y1");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("Input x2");
+            int x2 = int.Parse(Console.ReadLine());
+            Console.Write("Input y2");
+            int y2 = int.Parse(Console.ReadLine());
+            if (dx == x2 - x1)
+            {
+                if (dy == y2 - y1)
+                {
+                    if (Math.Abs(dx) >= Math.Abs(dy))
+                    {
+                        Console.Write(step = Math.Abs(dx));
+                    }
+                    else if ((Math.Abs(dx) >= Math.Abs(dy)))
+                    {
+                        Console.Write(dx /= step);
+                    }
+                    else if ((Math.Abs(dx) >= Math.Abs(dy)))
+                    {
+                        Console.Write(dy /= step);
+                    }
+                    else if ((Math.Abs(dx) >= Math.Abs(dy)))
+                    {
+                        Console.Write(x = x1);
+                    }
+                    else if ((Math.Abs(dx) >= Math.Abs(dy)))
+                    {
+                        Console.Write(y = y1);
+                    }
+                }
+                for (int i = 1; i <= step; i++)
+                {
+                    Console.Write("x={0},y={1}",dx,dy);
+                }
+            }
+            else
+                Console.Write("ERROR");
         }
     }
 }
